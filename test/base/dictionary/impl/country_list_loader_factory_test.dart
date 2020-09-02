ffd65573e94b23eb50b9b83cb16d44fd14f88d82
alexandershirokih <alexandrshirokih@mail.ru>
2020-09-02 21:57:22 +0300

    fix unit test

--- test/base/dictionary/impl/country_list_loader_factory_test.dart
+++ test/base/dictionary/impl/country_list_loader_factory_test.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
 import 'dart:io';
 
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:lets_play_cities/base/dictionary/impl/country_list_loader_factory.dart';
 
@@ -12,9 +14,12 @@ void main() {
     });
 
     test('sample data valid', () async {
-      final lines = await File("assets/data/countries.txt")
-          .readAsLines()
-          .then((value) => value.where((line) => line.isNotEmpty));
+      final lines = await rootBundle
+          .loadString("assets/data/countries.txt")
+          .asStream()
+          .expand((element) => LineSplitter().convert(element))
+          .where((line) => line.isNotEmpty)
+          .toList();
 
       expect(
           lines.every((l) => l.indexOf('|') < 0
