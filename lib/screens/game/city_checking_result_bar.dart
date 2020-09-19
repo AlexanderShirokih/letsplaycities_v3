c18efe680e2e3f52ff0438d34cc98d620307250a
alexandershirokih <alexandrshirokih@mail.ru>
2020-09-19 14:39:17 +0300

    add city correction dialog

--- lib/screens/game/city_checking_result_bar.dart
+++ lib/screens/game/city_checking_result_bar.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:lets_play_cities/base/game/management/word_checking_result.dart';
 import 'package:lets_play_cities/utils/string_utils.dart';
 import 'package:lets_play_cities/base/repos.dart';
@@ -14,6 +17,7 @@ class _CityCheckingResultBarState extends State<CityCheckingResultBar>
   AnimationController controller;
   Animation<double> animation;
   bool completed = false;
+  bool reverse = true;
 
   @override
   void initState() {
@@ -26,16 +30,18 @@ class _CityCheckingResultBarState extends State<CityCheckingResultBar>
       if (completed) return;
 
       if (status == AnimationStatus.completed) {
-        controller.reverse();
+        if (reverse) controller.reverse();
         completed = true;
+        reverse = true;
       } else if (status == AnimationStatus.dismissed) {
         controller.forward();
       }
     });
   }
 
-  void _triggerAnimation() {
+  void _triggerAnimation({bool reverse = true}) {
     completed = false;
+    this.reverse = reverse;
     controller.forward();
   }
 
@@ -47,35 +53,95 @@ class _CityCheckingResultBarState extends State<CityCheckingResultBar>
 
   @override
   Widget build(BuildContext context) {
+    final GameSessionRepository repo =
+        context.repository<GameSessionRepository>();
     return StreamBuilder<WordCheckingResult>(
-      stream: context.repository<GameSessionRepository>().wordCheckingResults,
+      stream: repo.wordCheckingResults,
       builder: (context, snapshot) {
-        String message = "";
-        if (snapshot.hasData && snapshot.data.isDescriptiveError()) {
+        if (!snapshot.hasData) return Container();
+
+        if (snapshot.data.isDescriptiveError()) {
           _triggerAnimation();
-          message = _translateWordCheckingResult(snapshot.data);
-        }
-        return SizeTransition(
-          sizeFactor: animation,
-          child: Container(
-            margin: EdgeInsets.all(8.0),
-            padding: EdgeInsets.symmetric(horizontal: 12.0),
-            height: 42.0,
-            color: Theme.of(context).secondaryHeaderColor,
-            child: Row(
-              children: [
-                Icon(Icons.warning),
-                SizedBox(width: 12.0),
-                Expanded(
-                  child: Text(message),
-                )
-              ],
+          return SizeTransition(
+            sizeFactor: animation,
+            child: _createNotificationBox(
+                context, _translateWordCheckingResult(snapshot.data)),
+          );
+        } else if (snapshot.data is Corrections) {
+          _triggerAnimation(reverse: false);
+          return SizeTransition(
+            sizeFactor: animation,
+            child: _showCorrectionsBox(
+              context,
+              repo,
+              (snapshot.data as Corrections)
+                  .corrections
+                  .toList(growable: false),
             ),
-          ),
-        );
+          );
+        }
+        return Container();
+        // Another way
       },
     );
   }
+
+  Widget _showCorrectionsBox(
+    BuildContext context,
+    GameSessionRepository repo,
+    List<String> corrections,
+  ) =>
+      Container(
+        padding: EdgeInsets.symmetric(horizontal: 12.0),
+        height: max(corrections.length * 64.0 + 30.0, 100),
+        color: Theme.of(context).secondaryHeaderColor,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(6.0),
+              child: Text(
+                "Варианты исправления",
+                style: Theme.of(context).textTheme.bodyText2,
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                padding: EdgeInsets.zero,
+                itemCount: min(corrections.length, 5),
+                itemBuilder: (context, index) => Card(
+                  elevation: 2,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(6),
+                  ),
+                  child: ListTile(
+                    onTap: () => repo.sendInputWord(corrections[index]),
+                    title: Text(corrections[index].toTitleCase()),
+                    leading: FaIcon(FontAwesomeIcons.city),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+
+  Widget _createNotificationBox(BuildContext context, String message) =>
+      Container(
+        margin: EdgeInsets.all(8.0),
+        padding: EdgeInsets.symmetric(horizontal: 12.0),
+        height: 42.0,
+        color: Theme.of(context).secondaryHeaderColor,
+        child: Row(
+          children: [
+            Icon(Icons.warning),
+            SizedBox(width: 12.0),
+            Expanded(
+              child: Text(message),
+            )
+          ],
+        ),
+      );
 
   static String _translateWordCheckingResult(WordCheckingResult data) {
     if (data is AlreadyUsed)
