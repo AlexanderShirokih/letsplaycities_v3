4b75bbc2cc94e1d4f7d95b742fbb54f3cc1a4e8b
alexandershirokih <alexandrshirokih@mail.ru>
2020-09-21 19:25:50 +0300

    move Search app bar to separated file

---
+++ lib/screens/common/search_app_bar.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+typedef OnTextChanged = void Function(String);
+
+/// AppBar with embedded search action
+/// Shows search text field after pressing search button
+/// When search text changes [onSearchTextChanged] calls.
+class SearchAppBar extends StatefulWidget implements PreferredSizeWidget {
+  final String title;
+  final String searchHint;
+  final OnTextChanged onSearchTextChanged;
+  final List<Widget> actions;
+
+  SearchAppBar({
+    this.title,
+    this.searchHint,
+    this.actions,
+    @required this.onSearchTextChanged,
+    Key key,
+  })  : assert(onSearchTextChanged != null),
+        preferredSize = Size.fromHeight(kToolbarHeight),
+        super(key: key);
+
+  @override
+  _SearchAppBarState createState() =>
+      _SearchAppBarState(title, searchHint, actions, onSearchTextChanged);
+
+  @override
+  final Size preferredSize;
+}
+
+class _SearchAppBarState extends State<SearchAppBar> {
+  final TextEditingController _controller = TextEditingController();
+  final String _title;
+  final String _searchHint;
+  final List<Widget> _actions;
+  final OnTextChanged _onSearchTextChanged;
+
+  bool isSearching = false;
+
+  _SearchAppBarState(
+    this._title,
+    this._searchHint,
+    this._actions,
+    this._onSearchTextChanged,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.addListener(
+        () => setState(() => _onSearchTextChanged(_controller.text)));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => AppBar(
+        leading: BackButton(onPressed: () {
+          if (isSearching)
+            setState(() {
+              isSearching = false;
+            });
+          else
+            Navigator.maybePop(context);
+        }),
+        title: isSearching
+            ? TextField(
+                controller: _controller,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: _searchHint,
+                ),
+              )
+            : Text(_title),
+        actions: isSearching
+            ? ([
+                  if (_controller.text.isNotEmpty)
+                    IconButton(
+                      icon: Icon(Icons.clear),
+                      onPressed: () => setState(() => _controller.clear()),
+                    ),
+                ].cast<Widget>() +
+                _actions)
+            : (_actions +
+                [
+                  IconButton(
+                    icon: Icon(Icons.search),
+                    onPressed: () => setState(() {
+                      isSearching = !isSearching;
+                    }),
+                  )
+                ].cast<Widget>()),
+      );
+}
--- lib/screens/main/citieslist/cities_list_screen.dart
+++ lib/screens/main/citieslist/cities_list_screen.dart
@@ -1,42 +1,27 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/material.dart';
 
 import 'package:lets_play_cities/base/cities_list/bloc/cities_list_bloc.dart';
 import 'package:lets_play_cities/base/cities_list/cities_list_entry.dart';
 import 'package:lets_play_cities/base/cities_list/cities_list_filter.dart';
 import 'package:lets_play_cities/screens/common/common_widgets.dart';
+import 'package:lets_play_cities/screens/common/search_app_bar.dart';
 import 'package:lets_play_cities/l18n/localization_service.dart';
-import 'package:lets_play_cities/utils/debouncer.dart';
 import 'package:lets_play_cities/utils/string_utils.dart';
+import 'package:lets_play_cities/utils/debouncer.dart';
 
-/// Screen that shows list of all database cities
+/// Screen that shows a list of all database cities
 class CitiesListScreen extends StatefulWidget {
   @override
   _CitiesListScreenState createState() => _CitiesListScreenState();
 }
 
 class _CitiesListScreenState extends State<CitiesListScreen> {
-  final TextEditingController _controller = TextEditingController();
   final Debouncer _cityFilterDebouncer = Debouncer(Duration(seconds: 1));
   final _citiesListBloc = CitiesListBloc();
 
-  bool isSearching = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller.addListener(() {
-      final text = _controller.text;
-      setState(() {
-        _cityFilterDebouncer.run(() => _citiesListBloc.add(
-            CitiesListFilteringEvent(CitiesListFilter(text.toLowerCase()))));
-      });
-    });
-  }
-
   @override
   void dispose() {
-    _controller.dispose();
     _cityFilterDebouncer.cancel();
     _citiesListBloc.close();
     super.dispose();
@@ -46,56 +31,27 @@ class _CitiesListScreenState extends State<CitiesListScreen> {
   Widget build(BuildContext context) {
     final l10n = context.repository<LocalizationService>();
     return Scaffold(
-        appBar: AppBar(
-          leading: BackButton(onPressed: () {
-            if (isSearching)
-              setState(() {
-                isSearching = false;
-              });
-            else
-              Navigator.maybePop(context);
-          }),
-          title: isSearching
-              ? TextField(
-                  controller: _controller,
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: l10n.citiesList['enter_city'],
-                  ),
-                )
-              : Text(l10n.citiesList['title']),
-          actions: isSearching
-              ? [
-                  if (_controller.text.isNotEmpty)
-                    IconButton(
-                      icon: Icon(Icons.clear),
-                      onPressed: () => setState(() {
-                        _controller.text = "";
-                      }),
-                    ),
-                  _createFilterButton()
-                ]
-              : [
-                  _createFilterButton(),
-                  IconButton(
-                    icon: Icon(Icons.search),
-                    onPressed: () => setState(() {
-                      isSearching = !isSearching;
-                    }),
-                  )
-                ],
-        ),
-        body: BlocBuilder(
-          cubit: _citiesListBloc,
-          builder: (context, state) {
-            if (state is CitiesListInitial) {
-              return LoadingView(l10n.citiesList['loading'].toString());
-            } else if (state is CitiesListDataState) {
-              return _displayData(l10n, state);
-            } else
-              throw ("Unexpected state $state");
-          },
-        ));
+      appBar: SearchAppBar(
+        title: l10n.citiesList['title'],
+        searchHint: l10n.citiesList['enter_city'],
+        actions: [_createFilterButton()],
+        onSearchTextChanged: (text) => setState(() {
+          _cityFilterDebouncer.run(() => _citiesListBloc.add(
+              CitiesListFilteringEvent(CitiesListFilter(text.toLowerCase()))));
+        }),
+      ),
+      body: BlocBuilder(
+        cubit: _citiesListBloc,
+        builder: (context, state) {
+          if (state is CitiesListInitial) {
+            return LoadingView(l10n.citiesList['loading'].toString());
+          } else if (state is CitiesListDataState) {
+            return _displayData(l10n, state);
+          } else
+            throw ("Unexpected state $state");
+        },
+      ),
+    );
   }
 
   Widget _displayData(LocalizationService l10n, CitiesListDataState data) =>
